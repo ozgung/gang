48f87241f7e1d6fc065f321019b284670ba8d0b4
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-02-15 18:36:23 +0200

    re-focus chat input

--- GangFrontend/client/assets/js/controllers/chat.js
+++ GangFrontend/client/assets/js/controllers/chat.js
@@ -5,13 +5,38 @@
   
 		.controller('ChatCtrl',function($scope,chat){
 			
-			$scope.message = '';
-			$scope.messages = chat.messages;
+			 /**
+		     * Message input field id
+		     * @const {string}
+		     */
+		    var MESSAGE_INPUT_ID = "message_input";
+
+		    function send() {
+
+		        //do send
+		        chat.sendMessage($scope.message);
+
+		        clearFocus();
+		    }
+
+		    function clearFocus() {
+		        //set focus to input field
+		        document.getElementById(MESSAGE_INPUT_ID).focus();
+
+		        //clear message input
+		        $scope.message = "";
+		    }
+
+		 	function init(){
+		 		$scope.messages = chat.messages;
+		 		clearFocus();
+		 	}
+
+		 	init();
+
+		 	//Exports
+			$scope.send = send;
 			
-			$scope.send = function(){
-				chat.sendMessage($scope.message);
-				$scope.message = '';
-			};
 			
 		});
 		

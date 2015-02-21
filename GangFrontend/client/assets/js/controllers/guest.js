6fffb9c4442f6005030b88143a58fed3a209b7d5
Özgün <ozgungenc@gmail.com>
2015-02-21 19:46:59 +0200

    redo changes

--- GangFrontend/client/assets/js/controllers/guest.js
+++ GangFrontend/client/assets/js/controllers/guest.js
@@ -11,14 +11,6 @@
 			$scope.login = function() {
 			
 				fb.login().then(function(response){
-					// ozgun
-					var access_token = localStorage.setItem('access-token')
-					console.log(access_token)
-					/*backend.request('login',{access-token: access_token}).then(function(data){
-					token.set(data.token);
-					$state.go('chat');
-					});*/
-					// ozgun
 					$state.go('account');
 				});
 			};

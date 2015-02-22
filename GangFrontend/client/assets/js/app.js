bbf42fac2eb230437a104c73f062ef975e040d2e
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-02-22 16:31:51 +0200

    010

--- GangFrontend/client/assets/js/app.js
+++ GangFrontend/client/assets/js/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    console.log("Gang version 009");
+    console.log("Gang version 010");
     var Gang = angular.module('application', [
         'ui.router',
         'ngAnimate',

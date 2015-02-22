756eba20e3422f0bf3caf2ec4600dc3818cdc0b8
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-02-22 16:58:51 +0200

    012

--- GangFrontend/client/assets/js/app.js
+++ GangFrontend/client/assets/js/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    console.log("Gang version 011");
+    console.log("Gang version 012");
     var Gang = angular.module('application', [
         'ui.router',
         'ngAnimate',
--- GangFrontend/client/assets/js/services/chat.js
+++ GangFrontend/client/assets/js/services/chat.js
@@ -1,41 +1,42 @@
-(function() {
+(function () {
 
-  'use strict';
+    'use strict';
 
-  angular.module('application')
-		
-		.service('chat',function($websocket,token){
-			
-			var self = this;
-			var activeChannel;
-			
-			var ws = $websocket('ws://ws.ganghq.com/ws?token='+token.get());
-			var messages = [];
-			
-			ws.onMessage(function(e) {
-				messages.push(JSON.parse(e.data));
-			});
-			
-			this.messages = messages;
-			
-			this.sendMessage = function(message){
-				
-				var channelName = '#channel';
-				
-				if(activeChannel){
-					channelName = '#' + activeChannel.id;
-				}
-				
-				
-        ws.send(JSON.stringify({
-					msg: message, 
-					channel: channelName
-				}));
-			};
-			
-			this.setActiveChannel = function(channel){
-        activeChannel = channel;
-			};
-			
-		});
+    angular.module('application')
+
+        .service('chat', function ($websocket, token) {
+
+            var self = this;
+            var activeChannel;
+
+            var ws = $websocket('ws://ws.ganghq.com/ws?token=' + token.get());
+            var messages = [];
+
+            ws.onMessage(function (e) {
+                messages.push(JSON.parse(e.data));
+            });
+
+            this.messages = messages;
+
+            this.sendMessage = function (message) {
+
+                var channelName = '#channel';
+
+                if (activeChannel) {
+                    channelName = '#' + activeChannel.id;
+                }
+
+
+                ws.send(JSON.stringify({
+                    msg: message,
+                    channel: channelName
+                }));
+            };
+
+            this.setActiveChannel = function (channel) {
+                console.log("setting actime channelId", channel);
+                activeChannel = channel;
+            };
+
+        });
 })();

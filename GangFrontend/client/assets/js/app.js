3f0ca63bfffaa065432be4dd2b5cb7edf2259dcf
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-02-26 15:42:27 +0200

    fix wrong profile picture at members

--- GangFrontend/client/assets/js/app.js
+++ GangFrontend/client/assets/js/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    console.log("Gang version 041");
+    console.log("Gang version 042");
     var Gang = angular.module('application', [
         'ui.router',
         'ngAnimate',

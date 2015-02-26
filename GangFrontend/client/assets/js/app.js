edaf60c182730fa43a470f19a4f8f36e8bd47368
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-02-26 14:00:28 +0200

    channel list now fetched with the api not FB

--- GangFrontend/client/assets/js/app.js
+++ GangFrontend/client/assets/js/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    console.log("Gang version 033");
+    console.log("Gang version 034");
     var Gang = angular.module('application', [
         'ui.router',
         'ngAnimate',

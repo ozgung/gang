703f8b31cca8afc4b443543032b159f3add8fb40
Özgün <ozgungenc@gmail.com>
2015-02-28 23:34:18 +0200

    more force

--- GangFrontend/gulpfile.js
+++ GangFrontend/gulpfile.js
@@ -34,7 +34,7 @@ var foundationJS = [
     'bower_components/fastclick/lib/fastclick.js',
     'bower_components/viewport-units-buggyfill/viewport-units-buggyfill.js',
     'bower_components/tether/tether.js',
-    'bower_components/angular/angular.js',
+    '!bower_components/angular/angular.js',
     'bower_components/angular-animate/angular-animate.js',
     '!bower_components/angular-sanitize/angular-sanitize.min.js',
     'bower_components/angulartics/dist/angulartics.min.js',
@@ -46,7 +46,7 @@ var foundationJS = [
     '!bower_components/foundation-apps/js/angular/app.js',
     'bower_components/angular-facebook/lib/angular-facebook.js',
     'bower_components/angular-scroll-glue/src/scrollglue.js',
-    'bower_components/angular-websocket/angular-websocket.js',
+    '!bower_components/angular-websocket/angular-websocket.js',
     '!bower_components/angular-emoji-filter-hd/dist/emoji.min.js'
 ];
 

6a90a8cf2527abf4a94694134dfb0f76a790c092
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-03-02 19:56:23 +0200

    78 online kullanicilar duzeldi

--- GangFrontend/client/assets/js/app.js
+++ GangFrontend/client/assets/js/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    console.log("Gang version 072");
+    console.log("Gang version 073");
     var Gang = angular.module('application', [
         'ui.router',
         'ngAnimate',

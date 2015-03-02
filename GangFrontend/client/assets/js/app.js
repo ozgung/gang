ada9122366f75ff68fba55aba6658fa005828fd6
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-03-02 16:49:47 +0200

    Facebook script removed from index.html

--- GangFrontend/client/assets/js/app.js
+++ GangFrontend/client/assets/js/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    console.log("Gang version 060");
+    console.log("Gang version 061");
     var Gang = angular.module('application', [
         'ui.router',
         'ngAnimate',

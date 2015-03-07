6005832fadb12d2f93d29157c432c1894031436d
Ilgaz Şumnulu <ilgaz@fikrimuhal.com>
2015-03-07 13:20:05 +0200

    #8 #17 needs implementation

--- GangFrontend/client/assets/js/controllers/account.js
+++ GangFrontend/client/assets/js/controllers/account.js
@@ -3,20 +3,41 @@
 
     angular.module('application')
 
-      .controller('AccountCtrl',function($scope,$state,chat,fb,user,teams,$window) {
-				
-				chat.setChannels(teams);
-				
-				$scope.user = user;
-				$scope.teams = teams;
-				
-				$scope.numberOfunreadMessages = chat.numberOfunreadMessages;
-				
-				$scope.logout = function(){
-				
-					$window.localStorage.removeItem('token');
-					$window.localStorage.removeItem('lastChannel')
-					$state.go('guest');
-				};
-      });
+        .controller('AccountCtrl', function ($scope, $state, chat, fb, user, teams, $window) {
+
+            chat.setChannels(teams);
+
+            $scope.user = user;
+            $scope.teams = teams;
+
+
+            /**
+             *todo
+             * @param team
+             * @returns {string}
+             */
+            $scope.getTeamColor = function (team) {
+                team.id
+                return "#c0392b"
+            };
+
+            /**
+             * todo
+             * @param team
+             * @returns {string}
+             */
+            $scope.getTeamShortName = function (team) {
+                team.name
+                return "IS"
+            };
+
+            $scope.numberOfunreadMessages = chat.numberOfunreadMessages;
+
+            $scope.logout = function () {
+
+                $window.localStorage.removeItem('token');
+                $window.localStorage.removeItem('lastChannel')
+                $state.go('guest');
+            };
+        });
 })();

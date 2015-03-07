467e06fafd0fa1523eae6c2ca4d121002fe7a3f2
Özgün <ozgungenc@gmail.com>
2015-03-07 16:52:15 +0200

    Closing #17 Team icon generation (color and letter). Added our flat color palette.

--- GangFrontend/client/assets/js/controllers/account.js
+++ GangFrontend/client/assets/js/controllers/account.js
@@ -11,14 +11,48 @@
             $scope.teams = teams;
 
 
+            function shortName (str, maxlen)
+            {
+				var words = str.split(" ");
+				var result = "";
+            	for (var word in words){
+            		if (result.length >= maxlen) return result; 
+            		var s = words[word].replace( /[^a-zA-Z]/, "");//remove non alpha
+            		if (s !=  '') result += s.charAt(0).toUpperCase();
+            	}
+            	return result;
+            }
+
+            /**
+			* color palette
+			* source: http://flatuicolors.co/
+            */
+            $scope.palette = ["#1ABC9C",//0
+            				  "#16A085",//1
+            				  "#F1C40F",//2
+            				  "#F39C12",//3
+            				  "#2ECC71",//4
+            				  "#27AE60",//5
+            				  "#E67E22",//6
+            				  "#D35400",//7
+            				  "#3498DB",//8
+            				  "#2980B9",//9
+            				  "#E74C3C",//10
+            				  "#C0392B",//11
+            				  "#9B59B6",//12
+            				  "#8E44AD",//13
+            				  "#34495E",//14
+            				  "#2C3E50"];//15
+
             /**
              *todo
              * @param team
              * @returns {string}
              */
             $scope.getTeamColor = function (team) {
-                team.id
-                return "#c0392b"
+                var id = team.id, result = "#34495E";
+                result = $scope.palette[id%16];
+                return result;
             };
 
             /**
@@ -27,8 +61,7 @@
              * @returns {string}
              */
             $scope.getTeamShortName = function (team) {
-                team.name
-                return "IS"
+                return shortName(team.name, 2);
             };
 
             $scope.numberOfunreadMessages = chat.numberOfunreadMessages;
